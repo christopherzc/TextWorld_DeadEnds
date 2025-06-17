263a1bdf7bbca715542595d620944f4bd711fe25
Christopher Cui <49284735+christopherzc@users.noreply.github.com>
2025-06-17 10:54:24 -0700

    Update world2inform7.py
    Removed "Textworld Banner" (To make things cleaner)

--- textworld/generator/inform7/world2inform7.py
+++ textworld/generator/inform7/world2inform7.py
@@ -450,24 +450,6 @@ class Inform7Game:
         source += textwrap.dedent(r"""
         Rule for printing the banner text:
             say "[fixed letter spacing]";
-            say "                    ________  ________  __    __  ________        [line break]";
-            say "                   |        \|        \|  \  |  \|        \       [line break]";
-            say "                    \$$$$$$$$| $$$$$$$$| $$  | $$ \$$$$$$$$       [line break]";
-            say "                      | $$   | $$__     \$$\/  $$   | $$          [line break]";
-            say "                      | $$   | $$  \     >$$  $$    | $$          [line break]";
-            say "                      | $$   | $$$$$    /  $$$$\    | $$          [line break]";
-            say "                      | $$   | $$_____ |  $$ \$$\   | $$          [line break]";
-            say "                      | $$   | $$     \| $$  | $$   | $$          [line break]";
-            say "                       \$$    \$$$$$$$$ \$$   \$$    \$$          [line break]";
-            say "              __       __   ______   _______   __        _______  [line break]";
-            say "             |  \  _  |  \ /      \ |       \ |  \      |       \ [line break]";
-            say "             | $$ / \ | $$|  $$$$$$\| $$$$$$$\| $$      | $$$$$$$\[line break]";
-            say "             | $$/  $\| $$| $$  | $$| $$__| $$| $$      | $$  | $$[line break]";
-            say "             | $$  $$$\ $$| $$  | $$| $$    $$| $$      | $$  | $$[line break]";
-            say "             | $$ $$\$$\$$| $$  | $$| $$$$$$$\| $$      | $$  | $$[line break]";
-            say "             | $$$$  \$$$$| $$__/ $$| $$  | $$| $$_____ | $$__/ $$[line break]";
-            say "             | $$$    \$$$ \$$    $$| $$  | $$| $$     \| $$    $$[line break]";
-            say "              \$$      \$$  \$$$$$$  \$$   \$$ \$$$$$$$$ \$$$$$$$ [line break]";
             say "[variable letter spacing][line break]";
             say "[objective][line break]".
 
